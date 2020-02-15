86cd2b255500f28306c211e6d00809ef27d82843
Joao C Barbosa <joaao@outlook.com>
2020-02-15 17:42:23 -0300

    Avoiding CORS Errors

--- app.js
+++ app.js
@@ -10,6 +10,19 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use((request, response, next) => {
+    response.header('Access-Control-Allow-Origin', '*');
+    response.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+
+    if (request.method === 'OPTIONS') {
+        response.header('Access-Control-Allow-Methods', 'PUT, POST, DELETE, PATCH, GET');
+        return response.status(200).json({});
+    }
+});
+
 
 // Routes which handle the requests
 app.use('/products', productsRoutes);
