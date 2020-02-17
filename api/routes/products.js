df7fee24c9c40752a0f650870df2839dba2da4af
João Otávio Crisóstomo Barbosa <jobarbosa@sfiec.org.br>
2020-02-17 16:31:05 -0300

    Add product in database

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const Product = require('../model/product');
+const mongoose = require('mongoose');
 
 router.get('/', (request, response, next) => {
     response.status(200).json({
@@ -8,10 +10,17 @@ router.get('/', (request, response, next) => {
 });
 
 router.post('/', (request, response, next) => {
-    const product = {
-        name: request.body.name,
+    const product = new Product({
+        _id : mongoose.Schema.Types.ObjectId,
+        name:  request.body.name,
         price: request.body.price
-    };
+    });
+
+    product.save()
+    .then(result => {
+        console.log(result)
+    })
+    .catch(err => console.log(err));
     response.status(200).json({
         message: "An order was created",
         createProduct: product
